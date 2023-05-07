988bdfd012132b5b5be0e269524802e5cd9dc4d4
amanjit-gill-data <amanjitgilldata@gmail.com>
2023-05-07 16:51:19 +1000

    got slime working with paragraphs

--- plugin/slime.lua
+++ plugin/slime.lua
@@ -1,30 +1,36 @@
--- automatically open ipython in built-in terminal
-
-vim.cmd("delmarks!")
+-- open ipython in built-in terminal
+-- set keymaps to send cells, paragraphs and lines
+-- set keymap to exit ipython and close terminal window
 
 vim.g.slime_target = 'neovim'
 vim.g.slime_dont_ask_default = 1
 vim.g.slime_bracketed_paste = 1
 vim.g.slime_cell_delimiter = '# %%'
 
-vim.keymap.set("n", "<Leader>ip", ":lua IPythonOpen()<CR>")
+vim.keymap.set("n", "<Leader>op", ":lua IPythonOpen()<CR>")
 
 function IPythonSendCell()
   vim.cmd("call slime#send_cell() | SlimeSend0 '\r'")
 end
 
---[[
-function IPythonSendParagraph()
+-- select and yank current paragraph, then send to ipython
+function IPythonSendParagraph() 
   vim.cmd("normal! vipy")
-  vim.fn.feedkeys(":let b:selected = getreg('\"0')\r", 't')
-  vim.b.cmd_string = "SlimeSend0 \'" .. vim.b.selected .. "\'" 
-  vim.cmd(vim.b.cmd_string)
+  vim.cmd("let b:selected = getreg('\"0')")
+  vim.cmd("SlimeSend0 b:selected")
+  vim.cmd("SlimeSend0 '\r'")
 end
---]]
 
-function IPythonSendParagraph()
-  vim.fn.feedkeys("normal! <C-c><C-c>")
-  vim.cmd('SlimeSend0 "\r"')
+function IPythonSendLine()
+  vim.cmd("SlimeSendCurrentLine")
+  vim.cmd("SlimeSend0 '\r'")
+end
+
+-- end python process then close all windows except for python file
+function IPythonExit()
+  vim.cmd("SlimeSend0 'exit()'")
+  vim.cmd("SlimeSend0 '\r'")
+  vim.cmd("only")
 end
 
 -- this code is based on https://github.com/hanschen/vim-ipython-cell/wiki
@@ -32,16 +38,22 @@ function IPythonOpen()
 
   -- open new terminal and start ipython; name the new window ipython
   vim.cmd("vnew | call termopen('ipython')")
-  name = python
 
   -- set job id, so slime knows where to send python code
   vim.g.slime_default_config = { jobid = vim.b.terminal_job_id }
- 
+
+  -- move cursor to bottom; it will stay there, ensuring latest output is 
+  -- always visible
+  vim.cmd("normal! G")
+
   -- return focus to previous window i.e. python script
   vim.cmd("wincmd p")
 
-  vim.keymap.set("n", "<C-s><C-p>", ":lua IPythonSendParagraph()<CR>") -- paragraph
-  vim.keymap.set("n", "<C-s><C-c>", ":lua IPythonSendCell()<CR>") -- cell
+  vim.keymap.set("n", "<C-c><C-c>", ":lua IPythonSendCell()<CR>") -- cell
+  vim.keymap.set("n", "<C-c><C-p>", ":lua IPythonSendParagraph()<CR>") -- paragraph
+  vim.keymap.set("n", "<C-c><C-l>", ":lua IPythonSendLine()<CR>") -- current line
+
+  vim.keymap.set("n", "<leader>cp", ":lua IPythonExit()<CR>")
 end
 
 
